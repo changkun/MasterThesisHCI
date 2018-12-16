b9c3ecad07325c394ff56e18949b0219284d0e9f
Changkun Ou <hi@changkun.us>
2018-12-16 14:33:04 +0100

    update

--- experiments/data/analysis/visualize/preprocessing.go
+++ experiments/data/analysis/visualize/preprocessing.go
@@ -24,90 +24,92 @@ type Target struct {
 }
 
 type Node struct {
-	Index       string  `json:"index"`
+	Index       int     `json:"id"`
 	URL         string  `json:"url"`
 	Group       int     `json:"group"`
 	StaySeconds float64 `json:"stay_seconds"`
 }
 type Link struct {
-	Source string `json:"source"`
-	Target string `json:"target"`
+	Source int `json:"source"`
+	Target int `json:"target"`
 	// SourceURL   string  `json:"source_url"`
 	// TargetURL   string  `json:"target_url"`
 	StaySeconds float64 `json:"stay_seconds"`
 }
 
 func main() {
-	target := Target{}
-	index := 0
+	for userid := 1; userid < 22; userid++ {
+		for taskid := 1; taskid < 10; taskid++ {
+			target := Target{}
+			index := 0
 
-	for i := 1; i < 2; i++ {
-		raw, err := ioutil.ReadFile(fmt.Sprintf("../dataset/%d.json", i))
-		if err != nil {
-			panic(err)
-		}
+			raw, err := ioutil.ReadFile(fmt.Sprintf("../dataset/%d.json", userid))
+			if err != nil {
+				panic(err)
+			}
 
-		var t []TaskClickstream
-		err = json.Unmarshal(raw, &t)
-		if err != nil {
-			panic(err)
-		}
+			var t []TaskClickstream
+			err = json.Unmarshal(raw, &t)
+			if err != nil {
+				panic(err)
+			}
 
-		nodes := map[string]struct {
-			index int
-			stay  float64
-		}{}
-		for _, obj := range t[0].Clickstream {
-			if v, ok := nodes[obj.PreviousURL]; !ok {
-				index++
-				nodes[obj.PreviousURL] = struct {
-					index int
-					stay  float64
-				}{
-					index: index,
-					stay:  obj.StaySeconds,
+			nodes := map[string]struct {
+				index int
+				stay  float64
+			}{}
+
+			for _, obj := range t[taskid-1].Clickstream {
+				if v, ok := nodes[obj.PreviousURL]; !ok {
+					index++
+					nodes[obj.PreviousURL] = struct {
+						index int
+						stay  float64
+					}{
+						index: index,
+						stay:  obj.StaySeconds,
+					}
+				} else {
+					v.stay = nodes[obj.PreviousURL].stay + obj.StaySeconds
 				}
-			} else {
-				v.stay = nodes[obj.PreviousURL].stay + obj.StaySeconds
-			}
-			if v, ok := nodes[obj.CurrentURL]; !ok {
-				index++
-				nodes[obj.CurrentURL] = struct {
-					index int
-					stay  float64
-				}{
-					index: index,
-					stay:  obj.StaySeconds,
+				if v, ok := nodes[obj.CurrentURL]; !ok {
+					index++
+					nodes[obj.CurrentURL] = struct {
+						index int
+						stay  float64
+					}{
+						index: index,
+						stay:  obj.StaySeconds,
+					}
+				} else {
+					v.stay += obj.StaySeconds
 				}
-			} else {
-				v.stay += obj.StaySeconds
+
+				link := Link{
+					Source:      nodes[obj.PreviousURL].index,
+					Target:      nodes[obj.CurrentURL].index,
+					StaySeconds: obj.StaySeconds,
+				}
+				target.Links = append(target.Links, link)
 			}
 
-			link := Link{
-				Source: fmt.Sprintf("%d", nodes[obj.PreviousURL].index),
-				Target: fmt.Sprintf("%d", nodes[obj.CurrentURL].index),
-				// SourceURL:   obj.PreviousURL,
-				// TargetURL:   obj.CurrentURL,
-				StaySeconds: obj.StaySeconds,
+			for k := range nodes {
+				node := Node{
+					Index:       nodes[k].index,
+					URL:         k,
+					Group:       userid,
+					StaySeconds: nodes[k].stay,
+				}
+				target.Nodes = append(target.Nodes, node)
 			}
-			target.Links = append(target.Links, link)
-		}
 
-		for k := range nodes {
-			node := Node{
-				Index:       fmt.Sprintf("%d", nodes[k].index),
-				URL:         k,
-				Group:       i,
-				StaySeconds: nodes[k].stay,
+			b, err := json.Marshal(target)
+			if err != nil {
+				panic(err)
 			}
-			target.Nodes = append(target.Nodes, node)
+
+			ioutil.WriteFile(fmt.Sprintf("js/clickstream-%d-%d.js", userid, taskid), []byte("var graph = "+string(b)), 0777)
 		}
 	}
 
-	b, err := json.Marshal(target)
-	if err != nil {
-		panic(err)
-	}
-
-	ioutil.WriteFile("clickstream.json", b, 0777)
 }
